621d97af64d0fd0870c10981263b8c835e67afd8
ALi <s.arme@ya.ru>
2020-05-14 21:46:24 +0430

    implement Simulated annealing

--- HillClimbing.py
+++ HillClimbing.py
@@ -26,14 +26,37 @@ def randomized_hill_climbing(n, minMax):
     m = (0, 0)
     for i in range(n):
         a = uniform(minMax[0], minMax[1])
-        print(a)
         res = Hill_Climbing(f, near, a)
         if m[1] < res[1]:
             m = res
     return m
 
 
+def annealing(fx, fxt, t):
+    if fxt >= fx:
+        return 1
+    else:
+        print(fx, fxt, t, math.e ** ((fxt - fx) / t))
+        return round(math.e ** ((fxt - fx) / t))
+
+
+def simulated_annealing(f, fn, g: float, t):
+    temp = t
+    current = g
+    while round(temp) > 0:
+        near = fn(current)
+        a = list(map(f, near))
+        for i in range(2):
+            p = annealing(f(current), a[i], temp)
+            if p == 1:
+                current = near[i]
+                break
+            temp -= 0.1
+    return current
+
+
 if __name__ == '__main__':
     v = Hill_Climbing(f, near, 2)
     vr = randomized_hill_climbing(10, (0.5, 2.5))
-    print(vr)
+    vsa = simulated_annealing(f, near, 0, 3)
+    print(vsa)

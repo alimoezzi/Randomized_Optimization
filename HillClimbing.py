f954ff8dc2d26b3b07b612c929052c71127d468f
ALi <s.arme@ya.ru>
2020-05-14 17:19:17 +0430

    Impelement Randomized Hill Climbing

--- HillClimbing.py
+++ HillClimbing.py
@@ -1,4 +1,7 @@
 import math
+from random import uniform
+
+from sqlalchemy.sql.functions import random
 
 f = lambda x: ((math.sin(10 * math.pi * x) / 2 * x) + (x - 1) ** 4) if 0.5 < x < 2.5 else 0
 
@@ -7,7 +10,7 @@ def near(n):
     return [n + 0.1, n - 0.1]
 
 
-def Hill_Climbing(f, fn, g: float) -> float:
+def Hill_Climbing(f, fn, g: float) -> (float, float):
     current = g
     while True:
         near = fn(current)
@@ -18,9 +21,21 @@ def Hill_Climbing(f, fn, g: float) -> float:
             else:
                 current = near[1]
         else:
-            return current
+            return (current, f(current))
+
+
+def randomized_hill_climbing(n, minMax):
+    m = (0, 0)
+    for i in range(n):
+        a = uniform(minMax[0], minMax[1])
+        print(a)
+        res = Hill_Climbing(f, near, a)
+        if m[1] < res[1]:
+            m = res
+    return m
 
 
 if __name__ == '__main__':
     v = Hill_Climbing(f, near, 2)
-    print(v)
+    vr = randomized_hill_climbing(10, (0.5, 2.5))
+    print(vr)
